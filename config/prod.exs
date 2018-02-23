58f4e409380440657990759922557407dc56f12c
Richard Dodd <rvdodd@outlook.com>
2018-02-23 19:55:26 +0000

    update prod config

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,14 @@ use Mix.Config
 # which you typically run after static files are built.
 config :platform, PlatformWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "platform-rd.herokuapp.com", port: 80],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+config :platform, Platform.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: S
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -61,4 +67,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
