ecd929efab16c6ccf6f145b6c2579ab756c40a3a
Richard Dodd <rvdodd@outlook.com>
2018-02-23 20:58:03 +0000

    created config files in wrong folder

--- config/prod.exs
+++ config/prod.exs
@@ -19,11 +19,11 @@ config :platform, PlatformWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
-  config :platform, Platform.Repo,
-    adapter: Ecto.Adapters.Postgres,
-    url: System.get_env("DATABASE_URL"),
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    ssl: true
+config :platform, Platform.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
