9c4b38112dff86b6778207e28d965c117922224c
Richard Dodd <rvdodd@outlook.com>
2018-02-26 20:16:24 +0000

    P73 Signing In

--- lib/platform_web/controllers/player_controller.ex
+++ lib/platform_web/controllers/player_controller.ex
@@ -18,8 +18,10 @@ defmodule PlatformWeb.PlayerController do
     case Accounts.create_player(player_params) do
       {:ok, player} ->
         conn
+        |> PlatformWeb.PlayerAuthController.sign_in(player)
         |> put_flash(:info, "Player created successfully.")
         |> redirect(to: player_path(conn, :show, player))
+  
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
