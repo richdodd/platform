61ecb826501656233536fb6e1d183ba61200851c
Richard Dodd <rvdodd@outlook.com>
2018-02-28 21:10:23 +0000

    Elm setup

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,3 +19,8 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+// Elm
+import Elm from "./main"
+const elmContainer = document.querySelector("#elm-container");
+if (elmContainer) Elm.Main.embed(elmContainer);
