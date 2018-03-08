0717cd08b2a45e00708b516dfb152bd4c9757457
Richard Dodd <rvdodd@outlook.com>
2018-03-08 21:22:39 +0000

    Elm Main Html

--- assets/elm/Main.elm
+++ assets/elm/Main.elm
@@ -1,8 +1,14 @@
 module Main exposing (..)
 
-import Html exposing (Html, text)
-
+import Html exposing (..)
+import Html.Attributes exposing (..)
 
 main : Html msg
 main =
-    text "Hello from Elm inside Phoenix!"
+    div [ class "games-index" ]
+    [ ul [ class "games-list" ]
+        [ li [] [ text "Platform Game" ]
+        , li [] [ text "Adventure Game" ]
+        ]
+    ]
+
