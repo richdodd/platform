5cd4a84dd7c9a2d8463d4ded9771b1f42a0211ca
Richard Dodd <rvdodd@outlook.com>
2018-03-08 21:36:26 +0000

    Elm split down into more functions

--- assets/elm/Main.elm
+++ assets/elm/Main.elm
@@ -1,14 +1,24 @@
 module Main exposing (..)
 
 import Html exposing (..)
+
 import Html.Attributes exposing (..)
 
 main : Html msg
 main =
-    div [ class "games-index" ]
-    [ ul [ class "games-list" ]
-        [ li [] [ text "Platform Game" ]
-        , li [] [ text "Adventure Game" ]
-        ]
+    div []
+    [ h1 [] [ text "Games" ]
+    , gamesIndex
     ]
 
+gamesIndex : Html msg
+gamesIndex =
+    div [ class "games-index" ] [ gamesList ]
+
+gamesList : Html msg
+gamesList =
+    ul [ class "games-list" ] [ gamesListItem ]
+
+gamesListItem : Html msg
+gamesListItem =
+    li [] [ text "Platform Game" ]
