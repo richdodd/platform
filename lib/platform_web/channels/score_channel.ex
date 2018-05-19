bd7002a0eccb07c8f615a486d43977d4a2e6f6a2
Richard Dodd <rvdodd@outlook.com>
2018-05-19 21:52:44 +0100

    P281 Create Gameplays

--- lib/platform_web/channels/score_channel.ex
+++ lib/platform_web/channels/score_channel.ex
@@ -1,11 +1,20 @@
 defmodule PlatformWeb.ScoreChannel do
     use PlatformWeb, :channel
 
-    def join("score:platformer", _payload, socket) do
+    def join("score:" <> game_slug, _payload, socket) do
+        game = Platform.Products.get_game_by_slug!(game_slug)
+        socket = assign(socket, :game_id, game.id)
         {:ok, socket}
     end
 
-    def handle_in("save_score", payload, socket) do
+    def handle_in("save_score", %{"player_score" => player_score} = payload, socket) do
+        payload = %{
+            player_score: player_score,
+            game_id: socket.assigns.game_id,
+            player_id: 1
+        }
+
+        Platform.Products.create_gameplay(payload)
         broadcast(socket, "save_score", payload)
         {:noreply, socket}
     end
